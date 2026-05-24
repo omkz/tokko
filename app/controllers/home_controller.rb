06441afb4a83f48ec8d2dab4b3c03206ebe27076
Kurnia Muhamad <omenkzz@gmail.com>
2026-05-24 07:40:49 +0700

    Remove dead home#search route and action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,4 @@ class HomeController < ApplicationController
                              .includes(products: [:product_variants, { images_attachments: :blob }])
   end
 
-  def search
-    @products = Product.search(params[:q]).order(created_at: :desc)
-    @pagy, @products = pagy(@products)
-    @products = @products.preload(:product_variants, images_attachments: :blob)
-  end
 end
