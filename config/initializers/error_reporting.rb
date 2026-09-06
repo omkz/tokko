9d47a249b62c45f55d71166b5e79ea73716d3769
Kurnia Muhamad <omenkzz@gmail.com>
2026-09-06 14:02:07 +0700

    Simplify error reporting

--- config/initializers/error_reporting.rb
+++
@@ -1,3 +0,0 @@
-# Production only: code isn't reloaded between requests in production, so
-# this registers exactly once per boot.
-Rails.error.subscribe(ErrorLogSubscriber.new) if Rails.env.production?
